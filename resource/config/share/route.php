a8b3a04619bcf4d2f8c4e80fc726abb41e6ee0b1
nuomi <nuomi@youzan.com>
2016-08-05 17:44:56 +0800

    fix default route to home

--- resource/config/share/route.php
+++ resource/config/share/route.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'default_route' => '/index',
+    'default_route' => '/home/',
     'default_controller' => 'index',
     'default_action' => 'index',
     'default_format' => 'html',
