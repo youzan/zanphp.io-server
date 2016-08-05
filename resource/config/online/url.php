7c92f8cd3903a6bb75f95fca2576b0e98d78ec05
nuomi <nuomi@youzan.com>
2016-08-05 17:46:12 +0800

    update url config

--- resource/config/online/url.php
+++ resource/config/online/url.php
@@ -4,8 +4,9 @@ return [
     'domain'      => 'zanphp.io',
     'base'        => '/',
     'static'      => 'http://s.zanphp.io/',
+    'doc'         => 'http://doc.zanphp.io/',
     'img'         => 'http://s.zanphp.io/img',
     'cdn_static'  => 'http://s.zanphp.io/',
-    'youzan'      => 'http://youzan.com/',
+    'youzan'      => 'https://youzan.com/',
     'koudaitong'  => 'http://koudaitong.com/'
 ];
--- resource/config/test/url.php
+++ resource/config/test/url.php
@@ -4,6 +4,7 @@ return [
     'domain'      => 'zanphp.io',
     'base'        => '/',
     'static'      => 'http://s.zanphp.io/',
+    'doc'         => 'http://doc.zanphp.io/',
     'img'         => 'http://s.zanphp.io/img',
     'cdn_static'  => 'http://s.zanphp.io/',
     'youzan'      => 'http://youzan.com/',
