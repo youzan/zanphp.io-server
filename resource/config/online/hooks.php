fa0faece51b9bf28ed897ecb86ba8d535b97fec4
nuomi <nuomi@youzan.com>
2016-08-07 17:26:31 +0800

    update hooks doc config

--- resource/config/online/hooks.php
+++ resource/config/online/hooks.php
@@ -8,10 +8,10 @@
 return [
     'doc' => [
         'secret' => '2X2u3PxaoX2dJbCb',
-        'src'    => '/srv/www/zanphp.io/doc/',
-        'build'  => '/srv/www/zanphp.io/doc/_book',
-        'dist'   => '/srv/www/zanphp.io/doc/_dist',
-        'backup' => '/srv/www/zanphp.io/doc/_backup',
+        'src'    => '/srv/www/zanphp.io/zan-doc/',
+        'build'  => '/srv/www/zanphp.io/zan-doc/_book',
+        'dist'   => '/srv/www/zanphp.io/doc/',
+        'backup' => '/srv/www/zanphp.io/_backup',
         'shell'  => 'gitbook build'
     ]
 ];
