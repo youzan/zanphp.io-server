3bad1a01f4da37ffa93b24c7a529225c9821a419
nuomi <nuomi@youzan.com>
2016-08-07 11:20:30 +0800

    add update doc hook

---
+++ src/Controller/Github/HooksController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nuomi
+ * Date: 8/6/16
+ * Time: 7:59 PM
+ */
+
+namespace Com\Youzan\ZanPhpIo\Controller\Github;
+
+
+use Com\Youzan\ZanPhpIo\Controller\Base\BaseController as Controller;
+
+class Hooks extends Controller
+{
+    public function updateDoc()
+    {
+
+    }
+
+}
